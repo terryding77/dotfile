6f83c38a4437de5098632dca1052ef2f1fe15f04
Terry Ding <terryding77@gmail.com>
2017-11-23 10:14:16 +0800

    1. add some useful plugins for vim         a. minibufexp for see exist used buffer         b. vim-bufkill for kill exist buffer and do not destory window layout         c. ZoomWin for expand specific pane to all window(ctrl-w o) 2. use default color for zsh in archlinux

--- zsh/custom/os-specific/Archlinux.zsh
+++ zsh/custom/os-specific/Archlinux.zsh
@@ -1,3 +1,5 @@
 eval $(dircolors -b ${ZSH_CUSTOM}/lib/dircolors.txt)
-zsh ${ZSH_CUSTOM}/lib/base16-tomorrow-night.sh
+#zsh ${ZSH_CUSTOM}/lib/base16-tomorrow-night.sh
 plugins+=(archlinux)
+
+alias o="xdg-open"
