3b2a23990004dec2b7c2a0cbcbe5b161ff699b90
terryding77 <terryding77@gmail.com>
2018-07-15 01:19:50 +0800

    remove  mac configs

--- zsh/custom/os-specific/Mac.zsh
+++ zsh/custom/os-specific/Mac.zsh
@@ -1,4 +1,2 @@
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 export PATH=$HOME/go/bin:$PATH
-export http_proxy="http://172.16.0.101:8086"
-export https_proxy="http://172.16.0.101:8086"
